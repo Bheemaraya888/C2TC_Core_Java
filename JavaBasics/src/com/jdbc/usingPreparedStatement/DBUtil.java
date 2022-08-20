c1fe599df78e054335f1e04c7b81ba01ae0a5eea
Bheemaraya <Bheemaraya@DESKTOP-UT1LUVE>
2022-08-20 14:59:25 +0530

    second commit

--- JavaBasics/src/com/jdbc/usingPreparedStatement/DBUtil.java
+++ JavaBasics/src/com/jdbc/usingPreparedStatement/DBUtil.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 public class DBUtil {
 	private static final String DB_DRIVER_CLASS="com.mysql.cj.jdbc.Driver";
 	private static final String DB_USERNAME="root";
-	private static final String DB_PASSWORD="admin";
+	private static final String DB_PASSWORD="root123";
 	private static final String DB_URL ="jdbc:mysql://localhost:3306/jdbcdb";
 	
 	private static Connection connection = null;
